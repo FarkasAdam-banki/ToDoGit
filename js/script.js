4d0e3eb5d34e775a2e5d9436af3918a65c94ac87
alexpinter <77269587+alexpinter@users.noreply.github.com>
2024-01-15 19:40:22 +0100

    alex
    javascript hibajavítás

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
+var toDoList = [];
+
 function clear() {
     var sections = document.getElementsByClassName("cont");
-    /*var sections = document.querySelectorAll("cont");*/
     for (var i = 0; i < sections.length; i++) {
         sections[i].style.display = "none";
     }
 }
 function start() {
     clear();
-    document.querySelector(".toDoList").style.display = "block";
 
+    document.querySelector(".toDoList").style.display = "block";
+    
 }
 
 function disappear() {
@@ -24,7 +26,13 @@ function appear() {
         bottombtns[i].style.display = "block";
     }
 }
-start();
+function addDel(name, state, deadline, color) {
+    name = document.querySelector(".toDoAddName").value = null;
+    state = document.querySelector(".stateSlider").value = 1;
+    deadline = document.querySelector('input[type="date"]').value = null;
+    color = document.querySelector(".colorPicker").value = "#ff0000";
+}
+
 
 
 var add = document.querySelector(".newbtn");
@@ -33,8 +41,6 @@ var del = document.querySelector(".deletebtn");
 var bottombtns = document.querySelectorAll(".bottombtn");
 var subtasks = document.querySelector(".addSubTask");
 
-
-
 add.addEventListener("click", function () {
     clear();
     document.querySelector(".toDoAdd").style.display = "block";
@@ -42,104 +48,33 @@ add.addEventListener("click", function () {
     disappear();
 
 })
-
 edit.addEventListener("click", function () {
     clear();
     document.querySelector(".toDoList").style.display = "block";
     document.querySelector(".title").innerHTML = "TEENDŐK";
     appear();
-})
 
+})
 del.addEventListener("click", function () {
     clear();
     document.querySelector(".toDoDel").style.display = "block";
-    document.querySelector(".title").innerHTML = "TEENDŐK";
+    document.querySelector(".title").innerHTML = "TEENDŐK-TÖRLÉSE";
     appear();
 })
 
+
 var subTaskIndex = 0;
-subtasks.addEventListener("click", function () {
 
-    let placesToAdd = document.querySelector(".toDoAddSubtask")
-    placesToAdd.innerHTML += '<div class="toDoAddSubTaskName subTask'+subTaskIndex+'">' +
-        '<div class="highlight"><img src="images/trash.png" alt="törlés" title="törlés" class="subTaskDelete'+subTaskIndex+'"></div>' +
+function addSubTaskElement() {
+    let placesToAdd = document.querySelector(".toDoAddSubtask");
+    let subTaskElement = document.createElement("div");
+    subTaskElement.classList.add("toDoAddSubTaskName", "subTask" + subTaskIndex);
+
+    subTaskElement.innerHTML =
+        '<div class="highlight2"><img src="images/trash.png" alt="törlés" title="törlés" class="subTaskDelete"></div>' +
         '<input type="text" class="toDoAddName" placeholder="Az alfeladat neve...">' +
         '<form class="toDoAddState">' +
-        '<input type="range" class="form-range custom-range" min="1" max="3" value="1" class="stateSlider" />' +
-        '</form>' +
-        '<div></div>' +
-        '<div></div>' +
-        '<div class="toDoAddStateText">' +
-        '<div class="leftState">Várakozo</div>' +
-        '<div class="centerState">folyamatban</div>' +
-        '<div class="rightState">elkészült</div>' +
-        '</div>' +
-        '</div>';
-        subTaskIndex++;
-
-
-})
-
-
-
-document.querySelector(".create-btn").addEventListener("click", function () {
-    // Adatok lekérése a HTML-ből
-    const todoNameInput = document.querySelector(".toDoAddName");
-    const todoStateInput = document.querySelector(".stateSlider");
-    const deadlineInput = document.querySelector(".toDoAddRightCenter input[type='date']");
-    const colorInput = document.querySelector(".colorPicker");
-
-    const todoName = todoNameInput ? todoNameInput.value : "";
-    const todoState = todoStateInput ? todoStateInput.value : "";
-    const deadline = deadlineInput ? deadlineInput.value : "";
-    const color = colorInput ? colorInput.value : "";
-    
-    const subtaskElements = document.querySelectorAll(".toDoAddSubtask .toDoAddSubTaskName");
-    const subtasks = [];
-    
-    subtaskElements.forEach(function (subtaskElement) {
-        const subtaskNameInput = subtaskElement.querySelector(".toDoAddName");
-        const subtaskStateInput = subtaskElement.querySelector(".stateSlider");
-    
-        const subtaskName = subtaskNameInput ? subtaskNameInput.value : "";
-        const subtaskState = subtaskStateInput ? subtaskStateInput.value : "";
-    
-        subtasks.push({ name: subtaskName, state: subtaskState });
-    });
-
-    // Objektumok létrehozása és a teendok tömbbe rakása
-    const todoObject = {
-        name: todoName,
-        state: todoState,
-        deadline: deadline,
-        color: color,
-        subtasks: subtasks
-    };
-
-    teendok.push(todoObject);
-    console.log("Elkészült objektumok:", teendok);
-    // Inputok nullázása
-    document.querySelector(".toDoAddName").value = "";
- 
-    document.querySelector(".toDoAddRightCenter input[type='date']").value = "";
-    
-
-    // Alfeladat inputok törlése
-    const subtaskContainer = document.querySelector(".toDoAddSubtask");
-    subtaskContainer.innerHTML = "";
-
-    // Üres alfeladat hozzáadása a frissítéshez
-    addEmptySubtask();
-});
-
-// Segédfüggvény az üres alfeladat hozzáadásához
-function addEmptySubtask() {
-    const subtaskContainer = document.querySelector(".toDoAddSubtask");
-    subtaskContainer.innerHTML += '<div class="toDoAddSubTaskName">' +
-        '<div class="highlight"><img src="images/trash.png" alt="törlés" title="törlés" class="subTaskDelete"></div>' +
-        '<input type="text" class="toDoAddName" placeholder="Az alfeladat neve...">' +
-        '<form class="toDoAddState">' +
-        '<input type="range" class="form-range custom-range" min="1" max="3" value="1" class="stateSlider" />' +
+        '<input type="range" class="form-range custom-range stateSlider stateSliderSub" min="1" max="3" value="1"/>' +
         '</form>' +
         '<div></div>' +
         '<div></div>' +
@@ -147,16 +82,69 @@ function addEmptySubtask() {
         '<div class="leftState">Várakozó</div>' +
         '<div class="centerState">folyamatban</div>' +
         '<div class="rightState">elkészült</div>' +
-        '</div>' +
         '</div>';
+
+    
+    subTaskElement.querySelector(".subTaskDelete").addEventListener("click", function () {
+        subTaskElement.remove();
+    });
+
+    placesToAdd.appendChild(subTaskElement);
+    subTaskIndex++;
 }
 
-// Kezdeti üres alfeladat hozzáadása
-addEmptySubtask();
+subtasks.addEventListener("click", addSubTaskElement);
 
-// A teendők tömb, ahol az elkészült objektumokat tároljuk
-const teendok = [];
+const todo1 = new Todo('Házimunka', 2, '2024-02-05', '#9b59b6');
+todo1.addSubtask(new Subtask('Porszívózás', 1));
+todo1.addSubtask(new Subtask('Mosogatás', 2));
+
+const todo2 = new Todo('Könyv olvasás', 3, '2024-03-01', '#e74c3c');
+
+const todo3 = new Todo('Bevásárlás', 1, '2024-02-01', '#3498db');
+todo3.addSubtask(new Subtask('Tejet venni', 1));
+todo3.addSubtask(new Subtask('Kenyér venni', 2));
+
+const todo4 = new Todo('Főzés', 2, '2024-02-10', '#f39c12');
+todo4.addSubtask(new Subtask('Recept kiválasztása', 1));
+todo4.addSubtask(new Subtask('Hozzávalók beszerzése', 2));
+
+const todo5 = new Todo('Sportolás', 1, '2024-02-15', '#2ecc71');
 
 
+toDoList.push(todo1, todo2, todo3, todo4, todo5);
 
+
+start();
+
+
+document.querySelector(".create-btn").addEventListener("click", function () {
+    const name = document.querySelector(".toDoAddName").value;
+    const state = document.querySelector(".stateSlider").value;
+    const deadline = document.querySelector('input[type="date"]').value;
+    const color = document.querySelector(".colorPicker").value;
+
+    if (name == "") {
+        document.querySelector(".alertDiv").innerHTML = '<div class="alert alert-danger alert-dismissible fade show" role="alert">' +
+            '<strong>Kérjük A létrehozáshoz adjon meg egy nevet!</strong><br> You should check in on some of those fields below.' +
+            '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' +
+            '</div>';
+
+    } else {
+        const todo = new Todo(name, state, deadline, color);
+
+        const subtaskContainers = document.querySelectorAll('.toDoAddSubTaskName');
+        subtaskContainers.forEach(subtaskContainer => {
+            const subtaskName = subtaskContainer.querySelector('.toDoAddName').value;
+            const subtaskState = subtaskContainer.querySelector('.stateSliderSub').value;
+            const subtask = new Subtask(subtaskName, subtaskState);
+            todo.addSubtask(subtask);
+        });
+
+        toDoList.push(todo);
+
+        console.log(toDoList);
+        
+    }
+});
 
